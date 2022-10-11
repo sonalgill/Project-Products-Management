9cb46f216c71e256661d24b8d39ad6efee1a6fad
sonalgill <sonalgill37@gmail.com>
2022-10-11 11:37:14 +0530

    post api

--- src/controller/aws.js
+++ src/controller/aws.js
@@ -1,22 +1,21 @@
-const aws = require('aws-sdk')
+const aws=require("aws-sdk")
 
 
-/////AWS ///////
 aws.config.update({
     accessKeyId: "AKIAY3L35MCRZNIRGT6N",
     secretAccessKey: "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",
     region: "ap-south-1"
 })
 
-let uploadFile= async ( file) =>{
+let uploadFile= async (file) =>{
    return new Promise( function(resolve, reject) {
-    // this function will upload file to aws and return the link
-    let s3= new aws.S3({apiVersion: '2006-03-01'}); // we will be using the s3 service of aws
+  
+    let s3= new aws.S3({apiVersion: '2006-03-01'}); 
 
     var uploadParams= {
         ACL: "public-read",
-        Bucket: "classroom-training-bucket",  //HERE
-        Key: "abc/" + file.originalname, //HERE 
+        Bucket: "classroom-training-bucket",  
+        Key: "abc/" + file.originalname, 
         Body: file.buffer
     }
 
@@ -30,34 +29,10 @@ let uploadFile= async ( file) =>{
         return resolve(data.Location)
     })
 
-    // let data= await s3.upload( uploadParams)
-    // if( data) return data.Location
-    // else return "there is an error"
+   
 
    })
 }
-
-// router.post("/write-file-aws", async function(req, res){
-
-//     try{
-//         let files= req.files
-//         if(files && files.length>0){
-//             //upload to s3 and get the uploaded link
-//             // res.send the link back to frontend/postman
-//             let uploadedFileURL= await uploadFile( files[0] )
-//             res.status(201).send({msg: "file uploaded succesfully", data: uploadedFileURL})
-//         }
-//         else{
-//             res.status(400).send({ msg: "No file found" })
-//         }
-        
-//     }
-//     catch(err){
-//         res.status(500).send({msg: err})
-//     }
-    
-// })
-
 
 
 module.exports = { uploadFile }
