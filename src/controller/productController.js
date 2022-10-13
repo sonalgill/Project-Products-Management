1dea125673805b31f7c38e8c31980c1f7a9e14bc
payal-chaudhary <payalsiwach96@gmail.com>
2022-10-13 15:29:43 +0530

    update&delete

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -1,4 +1,7 @@
+const { isValidObjectId } = require('mongoose')
+const { update } = require('../model/productModel')
 const productModel = require('../model/productModel')
+const { objectValue } = require('../validation/validations')
 const uploadFile = require('./aws').uploadFile
 const v = require("../validation/validations")
 
@@ -72,5 +75,145 @@ module.exports = {
         } catch (err) {
             return res.status(500).send({ status: false, Message: err.Message })
         }
+    }
+}
+///////////////update api/////////////
+
+const updateById = async function(req,res){
+    try{
+        const productId = req.params.productId;
+        const data = req.body;
+        const files = req.files
+        const{title, description, price, isFreeShipping, productImage, style, availableSizes, installments}= data
+        if (Object.keys(data).length==0){
+            return res.status(400).send({status:false, message:"Please provide data for update"})
+        }
+        if(!v.isValidObjectId(productId)){
+            return res.status(400).send({status:false, message:"Product Id is not vaild"})
+
+        }
+        let productExist = await productmodel.findById({_id:productId})
+        if(!productExist){
+            return res.status(404).send({status:false, message:"Product doesn't exist"})
+        }
+        if(productExist.isDeleted==true){
+            return res.status(400).send({status:false, message:"Product is deleted"})
+        }
+        let updateProduct = {isDeleted:false}
+        if(title)
+        if(!objectValue(title)){
+            return res.status(400).send({status:false, message:"Provide titile to be updated"})
+        }
+        let existTitle = await productmodel.findOne({title:title})
+        if(existTitle){
+            return res.status(400).send({status:false, message:"title already exist to be updated"})
+        }
+        updateProduct["title"]= title;
+
+        if(description){
+        if(!objectValue(description)){
+        return res.status(400).send({status:false, message:"Provide description to be updated"})
+            }
+        }
+        updateProduct["description"]= description;
+
+        if(price){
+        if(!objectValue(price)){
+            return res.status(400).send({status:false, message:"Provide price to be updated"})
+
+        }
+        }
+        updateProduct["price"]=price
+
+        if (currencyFormat) {
+            if (!objectValue(currencyFormat)) {
+                return res.status(400).send({ status: false, message: "Provide currency data to be updated" });
+            }
+            updateProduct["currencyFormat"] = currencyFormat;
+        }
+
+        if (isFreeShipping) {
+        if (!objectValue(isFreeShipping)) {
+            return res.status(400).send({ status: false, message: "Provide freeshipping data to be updated" });
+            }
+            updateProduct["isFreeShipping"] = isFreeShipping;
+        }
+        
+        if (style) {
+        if (!objectValue(style)) {
+            return res.status(400).send({ status: false, message: "Enter some data" });
+            }
+            updateProduct["style"] = style;
+        }
+
+        if (installments) {
+        if (!objectValue(installments)) {
+            return res.status(400).send({ status: false, message: "Enter some data" });
+            }
+
+            updateProduct["installments"] = installments;
+        }
+///enum validation////
+        let isValidEnum = (enm) =>{
+            var uniqueEnums = [...new Set(enm)];
+            const enumList = ["S", "XS", "M", "X", "L", "XXL", "XL"];
+            return enm.length === uniqueEnums.length && enm.every(e => enumList.includes(e));
+        }
+        ///////
+        if (availableSizes) {
+        if (!isValidData(availableSizes)) {
+           return res.status(400).send({ status: false, message: "Enter some data" });
+            }
+        let availableSize = availableSizes.split(',').map(s=>s.trim().toUpperCase())
+        if(!isValidEnum(available)){
+            return res.status(400).send({ status: false, message: `only allow S, XS, M, X, L, XXL, XL` })
+
+        }
+        // updateProduct["availableSize"]=availableSize
+
+        if(productImage){
+        if(files&& files.length>0){
+        productUrl = await uploadFile(files[0])}
+        updateProduct["productImage"]= productUrl
+        }
+
+     let finalUpdate = await productmodel.findOneAndUpadte({
+        _id:productId},{
+        $set:{
+            updateProduct},
+        
+        $push:{
+            availableSizes:{
+                $each: availableSize////testing
+            }
+        }
+    },
+        {new:true});
+     return res.status(200).send({status:true, message: finalUpdate })
+
+    }}catch(error){
+        return res.status(500).send({status:false, message:error.message})
+    }
+}
+
+////////////delete api/////////
+let deleteProductId= async function(req,res){
+    try{
+        let productId= req.params.productId
+        if(!v.isValidObjectId(productId)){
+            return res.status(400).send({status:false, message:"Invaild productId"})
+        }
+        let isExist = await productModel.findById({_id:productId})
+        if(!isExist){
+            return res.status(404).send({ status: false, message: "Product Id dosen't exists." });
+        }
+        if(isExist.isDeleted==true){
+            return res.status(400).send({ status: false, message: "Product is already deleted." });
+         }
+         let deleteProduct = await productModel.findOneAndUpdate({_id:productId}, {$set:{isDeleted:true, deletedAt:Date.now()}},{new:true})
+         return res.status(200).send({status:true, message:"Product deleted Successfully", data:deleteProduct})
+    }catch(error){
+        return res.status(500).send({status:false, message:error.message})
+
     }
 }
