25473fb2f0e6013f667b0f4403cc388728052c49
sonalgill <sonalgill37@gmail.com>
2022-10-13 15:23:20 +0530

    create product

--- src/validation/productValidation.js
+++ src/validation/productValidation.js
@@ -3,9 +3,9 @@ const v = require('./validations')
 module.exports = {
     createProduct: (req, res, next) => {
         try{
-            let {title, description, price, currencyId, currencyFormat, isFreeShipping, style, installments} = req.body
+            let {title, description, price, currencyId, currencyFormat, isFreeShipping, style, installments, availableSizes} = req.body
             if(!title || !title.trim().length)
-            return res.status(400).send({status: false, msg: "Title is Mandatory!"})
+            return res.status(400).send({status: false, msg: "Title is Mandatory. Also, it can't be an Empty String!"})
             if(!description || !description.trim().length)
             return res.status(400).send({status: false, msg: "Description is Mandatory!"})
             if(!price || !price.trim().length)
@@ -16,10 +16,16 @@ module.exports = {
             return res.status(400).send({status: false, msg:"CurrencyId can be INR only!"})
             if(currencyFormat && currencyFormat != '₹')
             return res.status(400).send({status: false, msg:"CurrencyFormat can be ₹ only!"})
-            if(isFreeShipping && (isFreeShipping != 'true' || isFreeShipping != 'false'))
-            return res.status(400).send({status: false, msg: "isFreeShipping can be true or false only!"})
+            if(isFreeShipping){
+            if(isFreeShipping != "true" && isFreeShipping != "false")
+            return res.status(400).send({status: false, msg: "isFreeShipping can be true or false only!"})}
             if(style && style.trim().length == 0)
-            return res.status.send({status: false, msg: "Style can not be an Empty String!"})
+            return res.status(400).send({status: false, msg: "Style can not be an Empty String!"})
+            if(!availableSizes)
+            return res.status(400).send({status: false, msg: "AvailableSizes is Mandatory!"})
+            if(installments){
+            if(!v.onlyNum(installments) || installments.length >= 3)
+            return res.status(400).send({status: false, msg: "Installments can be a 2 digit number Only!"})}
             next()
         }catch(e){
             res.status(500).send({status: false, msg: e.message})
