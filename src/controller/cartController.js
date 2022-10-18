ea192458421140cd5ba9f7a57861926e3403731d
sonalgill <sonalgill37@gmail.com>
2022-10-18 13:17:00 +0530

    feature 2

--- src/controller/cartController.js
+++ src/controller/cartController.js
@@ -141,34 +141,33 @@ module.exports = {
         } catch (err) {
             return res.status(500).send({ status: false, msg: err.message })
         }
+    },
+
+    deleteCart: async function(req,res){
+        try{
+            let userId= req.params.userId
+            if(!userId){
+                return res.status(400).send({status:false, message:"Please provide Userid"})
+            }
+            if(!v.isValidObjectId){
+                return res.status(400).send({status:false, message:"Please provide vaild userId"})
+            }
+             let findUser = await userModel.findById({userId})
+             if(!findUser){
+                return res.status(404).send({status:false, message:"user Doesn't exist"})
+             }
+             let findCart =await cartModel.findOne({userId})
+             if(!findCart){
+                return res.status(404).send({status:false, message:"No cart has found for this user id"})
+             }
+             if(findCart.items.length==0){
+                return res.status(404).send({status:false, message:"Cart Items has been deleted"})
+             }
+             let newCart = await cartModel.findByIdAndUpdate(findCart,{ $set:{items:[], totalItems:0, totalPrice:0}},{new:true})
+             return res.status(204).send({status:false, message: "Cart Deleted Successfully", data:newCart})
+    
+        }catch(error){
+            return res.status(500).send({status:false, message:error.message})
+        }
     }
 }  
-
-//////delete api/////////////////
-let deleteCart = async function(req,res){
-    try{
-        let userId= req.params.userId
-        if(!userId){
-            return res.status(400).send({status:false, message:"Please provide Userid"})
-        }
-        if(!v.isValidObjectId){
-            return res.status(400).send({status:false, message:"Please provide vaild userId"})
-        }
-         let findUser = await userModel.findById({userId})
-         if(!findUser){
-            return res.status(404).send({status:false, message:"user Doesn't exist"})
-         }
-         let findCart =await cartModel.findOne({userId})
-         if(!findCart){
-            return res.status(404).send({status:false, message:"No cart has found for this user id"})
-         }
-         if(findCart.items.length==0){
-            return res.status(404).send({status:false, message:"Cart Items has been deleted"})
-         }
-         let newCart = await cartModel.findByIdAndUpdate(findCart,{ $set:{items:[], totalItems:0, totalPrice:0}},{new:true})
-         return res.status(204).send({status:false, message: "Cart Deleted Successfully", data:newCart})
-
-    }catch(error){
-        return res.status(500).send({status:false, message:error.message})
-    }
-}
